153407bd31173cef8351af66440f5441716eb05e
avidito <av.leonardif@gmail.com>
2020-09-20 17:49:54 +0700

    menambahkan ekstraksi informasi artikel

--- web-crawler/repository_scraper/repository_scraper/spiders/unair.py
+++ web-crawler/repository_scraper/repository_scraper/spiders/unair.py
@@ -22,5 +22,21 @@ class UnairSpider(Spider):
     		absolute_url = url + "/view/year/{t}.html".format(t=self.tahun)
     		yield Request(url=absolute_url, callback=self.parse)
 
+    # METHOD PARSE UTAMA
     def parse(self, response):
-        pass
+        # Ekstraksi URL dari artikel dan request ke URL artikel
+        daftar_url_artikel = response.xpath('//p/a/@href').extract()
+        for url_artikel in daftar_url_artikel:
+            absolute_url_artikel = url_artikel
+            yield Request(url=absolute_url_artikel, callback=self.parse_info)
+
+    # METHOD PARSE INFO
+    def parse_info(self, response):
+        ringkasan = response.xpath('//p[@style="margin-bottom: 1em"]')
+        item = RepositoryScraperItem({
+                'judul': ringkasan.xpath('./em/text()').extract_first(),
+                'tahun': ringkasan.xpath('./text()[2]').extract_first(),
+                'divisi': response.xpath('//th[text()="Divisions:"]/parent::tr//a/text()').extract_first(),
+                'abstrak': response.xpath('//h2[text()="Abstract"]/following-sibling::p/text()').extract_first(),
+            })
+        yield item
