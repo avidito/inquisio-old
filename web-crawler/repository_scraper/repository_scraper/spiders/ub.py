c309c93bd8745008393aab34da8c00b2d3cd4afd
avidito <av.leonardif@gmail.com>
2020-09-21 08:47:09 +0700

    inisiasi spider ub

---
+++ web-crawler/repository_scraper/repository_scraper/spiders/ub.py
@@ -0,0 +1,37 @@
+from scrapy import Spider
+from scrapy import Request
+
+from datetime import datetime
+
+from repository_scraper.items import RepositoryScraperItem
+
+class UbSpider(Spider):
+    name = 'ub'
+    allowed_domains = ['repository.ub.ac.id']
+    start_urls = [
+    	'http://repository.ub.ac.id',
+    	]
+
+    # METHOD INISIASI
+    def __init__(self, tahun=None):
+    	self.tahun = tahun if (tahun is not None) else datetime.now().strftime("%Y")
+
+    # METHOD REQUESTS PERTAMA
+    def start_requests(self):
+    	for url in self.start_urls:
+    		absolute_url = url + "/view/year/{t}.html".format(t=self.tahun)
+    		yield Request(url=absolute_url, callback=self.parse)
+
+    # METHOD PARSE UTAMA
+    def parse(self, response):
+    	# Ekstraksi URL dari artikel dan request ke URL artikel
+    	daftar_url_artikel = response.xpath('//p/a/@href').extract()
+    	for url_artikel in daftar_url_artikel:
+    		absolute_url_artikel = url_artikel
+    		yield Request(url=absolute_url_artikel, callback=self.parse_info)
+
+    # METHOD PARSE INFO
+    def parse_info(self, response):
+    	pass
+
+
