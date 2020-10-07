5cdd63e1c71a8c894dae69f4ac4c07d03c345c55
avidito <av.leonardif@gmail.com>
2020-10-07 11:37:07 +0700

    menyesuaikan method api dengan fungsinya

--- web-crawler/berita_scraper/api/routes.py
+++ web-crawler/berita_scraper/api/routes.py
@@ -13,18 +13,18 @@ def indeks():
 
 # API untuk menjalankan Spider
 # Argumen : spider, kategori, tanggal, jumlah
-@app.route('/api/crawl/berita', methods=['GET'])
+@app.route('/api/crawl/berita', methods=['POST'])
 def mulai_crawling_berita():
 	
 	# Jika ada argumen 'spider', lakukan penugasan dan kembalikan hasil scraping
 	# Selainnya, kembalikan pesan untuk mengisi argumen
-	if ('spider' in request.args):
-		spider = request.args['spider']
+	if ('spider' in request.json):
+		spider = request.json['spider']
 
 		# Mengambil situs, tanggal, dan jumlah dari query
-		kategori = "default" if ('kategori' not in request.args) else request.args['kategori']
-		tanggal = "none" if ('tanggal' not in request.args) else request.args['tanggal']
-		jumlah = 0 if ('jumlah' not in request.args) else request.args['jumlah']
+		kategori = "default" if ('kategori' not in request.json) else request.json['kategori']
+		tanggal = "none" if ('tanggal' not in request.json) else request.json['tanggal']
+		jumlah = 0 if ('jumlah' not in request.json) else request.json['jumlah']
 
 		# Jalankan service
 		pesan = penugasan_spider(spider, kategori, tanggal, jumlah)
@@ -35,12 +35,12 @@ def mulai_crawling_berita():
 # API untuk mengekstraksi hasil crawling oleh Spider
 # Argumen : spider
 @app.route('/api/hasil/berita', methods=['GET'])
-def hasil_crawling_berita():
+@app.route('/api/hasil/berita/<spider>', methods=['GET'])
+def hasil_crawling_berita(spider=None):
 
 	# Jika ada argumen 'spider', lakukan penugasan dan kembalikan hasil scraping
 	# Selainnya, kembalikan pesan untuk mengisi argumen
-	if ('spider' in request.args):
-		spider = request.args['spider']
+	if (spider is not None):
 		pesan, hasil = ekstraksi_hasil(spider)
 
 		# Jika pesannya sudah selesai, kembalikan hasil
