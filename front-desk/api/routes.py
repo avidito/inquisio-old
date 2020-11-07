dc95035dc0b1dd825a7d45ffe9b6ea4a1672445a
leniangraeni <leni_angraeni@apps.ipb.ac.id>
2020-11-07 20:48:26 +0800

    implementasi front-desk bagian pesan

--- front-desk/api/routes.py
+++ front-desk/api/routes.py
@@ -1,11 +1,28 @@
 from flask import request, jsonify
 
 from api import app
-from api.services import index_process
+from api.services import pesan
 
 
-@app.route("/", methods=["GET"])
-def index():
-	data = request.args.get("data")
-	result = index_process(data)
-	return jsonify(result)
+
+@app.route("/api/task", methods=["POST"])
+def task():
+	tugas = request.json.get("tugas")
+	parameter = request.json.get("parameter")
+
+	if tugas: 
+		if tugas == "pesan":
+			result = pesan(parameter)
+			return jsonify(result)
+
+		else:
+			return jsonify({
+					"status": "ditolak",
+					"pesan": "tugas tidak sesuai"
+				})
+	else:
+		return jsonify({
+				"status": "ditolak",
+				"pesan": "tugas tidak boleh kosong"
+
+			})
